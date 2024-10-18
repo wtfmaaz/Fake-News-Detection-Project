ee6840ab4295a6a2108738ef8f111e51b381d4aa
maaz khan <157114487+wtfmaaz@users.noreply.github.com>
2024-10-18 20:40:19 +0530

    Update Fake_News_Detection.py

--- Fake_News_Detection.py
+++ Fake_News_Detection.py
@@ -1,13 +1,6 @@
 {
   "nbformat": 4,
   "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
       "display_name": "Python 3"
     },
     "language_info": {
