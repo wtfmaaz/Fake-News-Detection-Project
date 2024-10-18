39ff7bb92fd428870f0fde9786d581b0b89f04f3
maaz khan <157114487+wtfmaaz@users.noreply.github.com>
2024-10-18 22:11:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ vocab = pickle.load(open('tfidf_vocab.pkl', 'rb'))
 import pickle
 filename = 'fake_news_model.pkl'
 pickle.dump(model, open(filename, 'wb'))
-pickle.dump(Tfidf_vectorizer.vocabulary_, open('tfidf_vocab.pkl', 'wb'))
+pickle.dump(Tfidfvectorizer.vocabulary_, open('tfidf_vocab.pkl', 'wb'))
 
 tfidf = TfidfVectorizer(vocabulary=vocab)
 ps = PorterStemmer()
