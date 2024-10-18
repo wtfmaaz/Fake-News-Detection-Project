341b6301dc0ebad6b8735825d14ba57e4b1f780d
maaz khan <157114487+wtfmaaz@users.noreply.github.com>
2024-10-18 21:23:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1JR0tXUTSjvBJikK91OgQMjlJF_28hL4O
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
