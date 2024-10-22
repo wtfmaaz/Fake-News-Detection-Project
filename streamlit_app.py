639f10a981c63f188d4862e03ca3fd56251a1308
maaz khan <157114487+wtfmaaz@users.noreply.github.com>
2024-10-22 15:48:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ Original file is located at
 import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from nltk.stem.porter import PorterStemmer
 import re
@@ -21,6 +22,10 @@ model = pickle.load(open('fake_news_model.pkl', 'rb'))
 vocab = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
 
 tfidf = TfidfVectorizer(vocabulary=vocab)
+X_train, X_test, y_train, y_test = train_test_split(news['cleaned_text'], news['label'], test_size=0.2, random_state=42)
+tfidf_vectorizer = TfidfVectorizer(max_features=5000)
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
 tfidf.fit
 ps = PorterStemmer()
 stop_words = stopwords.words('english')
